708db0fe1601d2bebe35b77da1acc3a54caf64fe
SSILHR\uqayyum <uqayyum@ssidecisions.com>
2019-05-21 17:13:31 +0500

    helper method to generate code

--- OTP_Generator/Program.cs
+++ OTP_Generator/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OtpNet;
+using System;
 
 namespace OTP_Generator
 {
@@ -7,6 +8,23 @@ namespace OTP_Generator
         static void Main(string[] args)
         {
             Console.WriteLine("Kick Off!");
+        }
+
+        //Helper Methods
+        static private int generateOTP(string key)
+        {
+            int code = -1;
+            try
+            {
+                var totp = new Totp(Base32Encoding.ToBytes(key));
+                while (totp.RemainingSeconds() < 29) { }
+                code = Int32.Parse(totp.ComputeTotp(DateTime.UtcNow));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return code;
         }
     }
 }
